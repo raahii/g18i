444eebc6e997196e5bad8f70a887bc0780ff04d0
raahii <piyo56.net@gmail.com>
2018-07-28 11:41:40 +0900

    add endpoints

--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 import (
   "fmt"
   "log"
-  "net/http"
   "os"
+  "net/http"
+  "github.com/gorilla/mux"
 )
 
+func GetRecipes(w http.ResponseWriter, r *http.Request) {}
+func GetRecipe(w http.ResponseWriter, r *http.Request) {}
+func CreateRecipe(w http.ResponseWriter, r *http.Request) {}
+func UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
+func DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
+
 func main() {
+  router := mux.NewRouter()
+  router.HandleFunc("/recipes", GetRecipes).Methods("GET")
+  router.HandleFunc("/recipes/{id}", GetRecipe).Methods("GET")
+  router.HandleFunc("/recipes", CreateRecipe).Methods("POST")
+  router.HandleFunc("/recipes/{id}", UpdateRecipe).Methods("POST")
+  router.HandleFunc("/recipes/{id}", DeleteRecipe).Methods("DELETE")
+
   port := os.Getenv("PORT")
   if port == "" {
     log.Fatal("$PORT must be set")
   }
-
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":"+port, nil)
+  log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
